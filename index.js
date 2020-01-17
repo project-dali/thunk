8b53edfe66aa995b254479098932146c14c1fd0c
bschenerman <ben.schenerman@gmail.com>
2020-01-17 16:20:27 -0500

    🔧 Enforce consistent js style

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*eslint-env node */
 const express = require('express');
 const app = express();
 const fs = require('fs');
@@ -13,8 +14,8 @@ let round = 0;
 let rndResponses = 0;
 let jsonDataGlobal = '';
 const jsonInit = {
-    "rounds": []
-}
+	'rounds': []
+};
 const db = require('./db');
 const secret = require('./db-secret');
 
@@ -22,20 +23,21 @@ let connection = db.createCon(secret.dbCredentials);
 let query = '';
 
 const initializeJSON = (location, data) => {
-    try {
-        // write initial file structure
-        fs.writeFileSync(location, JSON.stringify(data, null, 2));
-    } catch (err) {
-        throw err;
-    }
-}
+	try {
+		// write initial file structure
+		fs.writeFileSync(location, JSON.stringify(data, null, 2));
+	} catch (err) {
+		console.log('we failed');
+		throw err;
+	}
+};
 
 initializeJSON('data.json', jsonInit);
 
 app.use('/static', express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + htmlDir + '/landing.html');
+	res.sendFile(__dirname + htmlDir + '/landing.html');
 });
 
 /**
@@ -43,132 +45,134 @@ app.get('/', function (req, res) {
  * @returns string
  */
 const createDeviceID = () => {
-    let deviceID = '';
-    for (let i = 0; i < 8; i++) {
-        deviceID += String(Math.floor(Math.random() * 10));
-    }
-    return deviceID;
-}
+	let deviceID = '';
+	for (let i = 0; i < 8; i++) {
+		deviceID += String(Math.floor(Math.random() * 10));
+	}
+	return deviceID;
+};
 
 io.on('connection', function (socket) {
 
-    // generate deviceID
-    let deviceID = createDeviceID();
-    // set deviceID in db
-    query = '';
-    query += 'INSERT INTO thunk.device (id)';
-    query += `VALUES (${deviceID})`
-    // send query to db
-    // if value is already used, generate a new one    
+	// generate deviceID
+	let deviceID = createDeviceID();
+	// set deviceID in db
+	query = '';
+	query += 'INSERT INTO thunk.device (id)';
+	query += `VALUES (${deviceID})`;
+	// send query to db
+	// if value is already used, generate a new one    
 
-    socket.on('join game', function () {
-        // emit the room join page back to the socket
-        socket.emit('advance to: join form', deviceID);
-    })
+	socket.on('join game', function () {
+		// emit the room join page back to the socket
+		socket.emit('advance to: join form', deviceID);
+	});
 
-    socket.on('chat message', function (msg) {
+	socket.on('chat message', function (msg) {
 
-        //increment index of total user responses in this round
-        rndResponses++;
+		//increment index of total user responses in this round
+		rndResponses++;
 
-        let msgData = {
-            response_num: rndResponses,
-            message: msg
-        };
+		let msgData = {
+			response_num: rndResponses,
+			message: msg
+		};
 
-        fs.readFile('data.json', 'utf8', function (err, currentData) {
-            if (err) throw err;
+		fs.readFile('data.json', 'utf8', function (err, currentData) {
+			if (err) throw err;
 
-            // read data.json as json object
-            jsonDataGlobal = JSON.parse(currentData);
+			// read data.json as json object
+			jsonDataGlobal = JSON.parse(currentData);
 
-            // get most recent round obj and push to its responses arr
-            let currentRoundParent = jsonDataGlobal.rounds[jsonDataGlobal.rounds.length - 1];
-            currentRoundParent.responses.push(msgData);
+			// get most recent round obj and push to its responses arr
+			let currentRoundParent = jsonDataGlobal.rounds[jsonDataGlobal.rounds.length - 1];
+			currentRoundParent.responses.push(msgData);
 
-            try {
-                // rewrite data.json with the updated global json obj
-                fs.writeFileSync('data.json', JSON.stringify(jsonDataGlobal, null, 2));
+			try {
+				// rewrite data.json with the updated global json obj
+				fs.writeFileSync('data.json', JSON.stringify(jsonDataGlobal, null, 2));
 
-                // console.log('The User Response was appended to the file!');
-            } catch (err) {
-                throw err;
-            }
-        });
+				// console.log('The User Response was appended to the file!');
+			} catch (err) {
+				console.log('we failed');
+				throw err;
+			}
+		});
 
-        // console.log('user msg: ' + msg);
-        io.emit('chat message', msg);
-    });
+		// console.log('user msg: ' + msg);
+		io.emit('chat message', msg);
+	});
 
-    socket.on('new game', function () {
-        round = 1;
-        usedPrompts = [];
-        rndResponses = 0;
-        // console.log('New game started!');
-    })
+	socket.on('new game', function () {
+		round = 1;
+		let usedPrompts = [];
+		rndResponses = 0;
+		// console.log('New game started!');
+	});
 
-    socket.on('new round', function () {
+	socket.on('new round', function () {
 
-        rndResponses = 0;
+		rndResponses = 0;
 
-        query = `SELECT * FROM prompt
+		query = `SELECT * FROM prompt
         ORDER BY RAND()
         LIMIT 1`;
-        db.sendQuery(query, connection, (results) => {
-            // console.log(results)
-            let now = new Date();
+		db.sendQuery(query, connection, (results) => {
+			// console.log(results)
+			let now = new Date();
 
-            let roundData = {
-                time: now.toISOString(),
-                round: round,
-                prompt_id: results[0].id,
-                prompt: results[0].prompt,
-                responses: []
-            };
+			let roundData = {
+				time: now.toISOString(),
+				round: round,
+				prompt_id: results[0].id,
+				prompt: results[0].prompt,
+				responses: []
+			};
 
-            let responseMsg = 'Time ' + now.toLocaleString() + '; Round ' + roundData.round +
-                '; prompt_id=' + roundData.prompt_id + '; prompt: ' + roundData.prompt;
+			let responseMsg = 'Time ' + now.toLocaleString() + '; Round ' + roundData.round +
+				'; prompt_id=' + roundData.prompt_id + '; prompt: ' + roundData.prompt;
 
-            fs.readFile('data.json', 'utf8', function (err, currentData) {
-                if (err) throw err;
+			fs.readFile('data.json', 'utf8', function (err, currentData) {
+				if (err) throw err;
 
-                // read data.json as json object
-                jsonDataGlobal = JSON.parse(currentData);
+				// read data.json as json object
+				jsonDataGlobal = JSON.parse(currentData);
 
-                // push new round obj to rounds arr
-                jsonDataGlobal.rounds.push(roundData);
+				// push new round obj to rounds arr
+				jsonDataGlobal.rounds.push(roundData);
 
-                try {
-                    // rewrite data.json with the updated global json obj
-                    fs.writeFileSync('data.json', JSON.stringify(jsonDataGlobal, null, 2));
+				try {
+					// rewrite data.json with the updated global json obj
+					fs.writeFileSync('data.json', JSON.stringify(jsonDataGlobal, null, 2));
 
-                    // console.log('The User Response was appended to the file!');
-                } catch (err) {
-                    throw err;
-                }
-            });
+					// console.log('The User Response was appended to the file!');
+				} catch (err) {
+					console.log('we failed');
+					throw err;
+				}
+			});
 
-            // console.log(responseMsg)
-            io.emit('chat message', responseMsg);
-        });
-        round++;
+			// console.log(responseMsg)
+			io.emit('chat message', responseMsg);
+		});
+		round++;
 
-    });
+	});
 
-    socket.on('send survey', function () {
-        let responseMsg = 'survey mode initiated';
-        // console.log(responseMsg);
+	socket.on('send survey', function () {
+		let responseMsg = 'survey mode initiated';
+		// console.log(responseMsg);
 
-        io.emit('chat message', responseMsg)
-        io.emit('take survey');
-    });
+		io.emit('chat message', responseMsg);
+		io.emit('take survey');
+	});
 
-    socket.on('disconnect', function () {
-        // console.log('user disconnected');
-    });
+	socket.on('disconnect', function () {
+		// console.log('user disconnected');
+	});
 
 });
 
 http.listen(port, function () {
-    console.log(`listening on /:${port}`);
+	console.log(`listening on /:${port}`);
 });

c39dde79fb9c2d28fc5345199d704a7e0124580d
Ben Schenerman <ben.schenerman@gmail.com>
2020-01-19 12:40:27 -0500

    🔨 Remove prototype code

--- index.js
+++ index.js
@@ -7,27 +7,9 @@ const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 const htmlDir = '/html';
 
-let round = 0;
-let rndResponses = 0;
-let jsonDataGlobal = '';
-const jsonInit = {
-	'rounds': []
-};
 const db = require('./db');
 const secret = require('./db-secret');
 let connection = db.createCon(secret.dbCredentials);
-
-const initializeJSON = (location, data) => {
-	try {
-		// write initial file structure
-		fs.writeFileSync(location, JSON.stringify(data, null, 2));
-	} catch (err) {
-		console.log('we failed');
-		throw err;
-	}
-};
-
-initializeJSON('data.json', jsonInit);
 
 app.use('/static', express.static(__dirname + '/public'));
 
@@ -49,7 +31,7 @@ const createDeviceID = () => {
 
 io.on('connection', function (socket) {
 	console.log('new connection');
-	console.log('');
+	console.log(socket);
 	const createDeviceEntry = (deviceID) => {
 		let query = 'INSERT INTO thunk.device (id)';
 		query += `VALUES (${deviceID});`;
@@ -80,48 +62,6 @@ io.on('connection', function (socket) {
 	socket.on('join game', function () {
 		// emit the room join page back to the socket
 		socket.emit('advance to: join form', deviceID);
-	});
-
-	socket.on('chat message', function (msg) {
-
-		//increment index of total user responses in this round
-		rndResponses++;
-
-		let msgData = {
-			response_num: rndResponses,
-			message: msg
-		};
-
-		fs.readFile('data.json', 'utf8', function (err, currentData) {
-			if (err) throw err;
-
-			// read data.json as json object
-			jsonDataGlobal = JSON.parse(currentData);
-
-			// get most recent round obj and push to its responses arr
-			let currentRoundParent = jsonDataGlobal.rounds[jsonDataGlobal.rounds.length - 1];
-			currentRoundParent.responses.push(msgData);
-
-			try {
-				// rewrite data.json with the updated global json obj
-				fs.writeFileSync('data.json', JSON.stringify(jsonDataGlobal, null, 2));
-
-				// console.log('The User Response was appended to the file!');
-			} catch (err) {
-				console.log('we failed');
-				throw err;
-			}
-		});
-
-		// console.log('user msg: ' + msg);
-		io.emit('chat message', msg);
-	});
-
-	socket.on('new game', function () {
-		round = 1;
-		let usedPrompts = [];
-		rndResponses = 0;
-		// console.log('New game started!');
 	});
 
 	socket.on('new round', function () {
@@ -172,15 +112,6 @@ io.on('connection', function (socket) {
 		round++;
 
 	});
-
-	socket.on('send survey', function () {
-		let responseMsg = 'survey mode initiated';
-		// console.log(responseMsg);
-
-		io.emit('chat message', responseMsg);
-		io.emit('take survey');
-	});
-
 });
 
 http.listen(port, function () {
